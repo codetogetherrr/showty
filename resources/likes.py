1f79585ab76220887bd254107cce38f49e57652f
Sylwia Krakowska <34857504+codetogetherrr@users.noreply.github.com>
2019-02-18 13:33:22 +0100

    Update likes.py

--- resources/likes.py
+++ resources/likes.py
@@ -40,5 +40,5 @@ class Likes_All(Resource):
         data = Likes_All.parser.parse_args()
      
         #likes_count = LikesModel.count_likes_post_id(data['post_id']).sum(status)
-        return {'all_likes_post_id': [x.json() for x in LikesModel.find_by_post_id(data['post_id'])]}
+        return {'likes': [x.json() for x in LikesModel.find_by_post_id(data['post_id'])]}
     
