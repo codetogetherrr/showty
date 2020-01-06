846cbce27f43bbb6fdc1ed865413ac1ead445d80
Maciej Chudzik <chudus@icloud.com>
2020-01-06 15:55:21 +0100

    adding marshamallow

--- schemas/user.py
+++ schemas/user.py
@@ -3,9 +3,15 @@ from models.user import UserModel
 
 
 
+
 class UserSchema(ma.ModelSchema):
 
     class Meta:
         model = UserModel
         load_only = ("password",)
+
+    email = ma.fields.Email(missing=None, required=True)
+    password = ma.fields.Email(missing=None, required=True)
+    login = ma.fields.Email(missing=None, required=True)
+
 
