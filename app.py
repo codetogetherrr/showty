088222bad20007ba9786c199202e9b3ec2a2508c
Sylwia Krakowska <34857504+codetogetherrr@users.noreply.github.com>
2020-03-07 18:51:02 +0100

    changed like

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ api.add_resource(Search, '/search')
 api.add_resource(Subscribe, '/subscribe', methods=['POST'], endpoint='subscribe')
 api.add_resource(Subscribe, '/subscribe/<hashtag>/<subscriber>', methods=['GET'], endpoint='checksubscription')
 api.add_resource(Subscriptions, '/subscriptions/<hashtag>')
-api.add_resource(Feed, '/feed/<page>')
+api.add_resource(Feed, '/feed/<page>/<per_page>')
 
 
 if __name__ == '__main__':
