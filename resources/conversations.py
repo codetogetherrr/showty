461f96c54080a17658ed35683bad98e4d7fcdc96
Maciej Chudzik <chudus@icloud.com>
2020-06-07 14:33:57 +0200

    added conversations

--- resources/conversations.py
+++ resources/conversations.py
@@ -8,4 +8,4 @@ message_schema = MessageSchema()
 
 class Conversations(Resource):
     def get(self, login):
-        return {'conversation_addressees': [x.receiver for x in MessageModel.find_conversation_addressees(login)]}
+        return {'conversations': [{x.receiver: [message_schema.dump(y) for y in MessageModel.find_by_pair(x.receiver, login)]} for x in MessageModel.find_conversation_addressees(login)]}
