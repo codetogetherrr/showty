2ec5a0bf7538662e311cc6755baecc412c2e3040
Sylwia Krakowska <34857504+codetogetherrr@users.noreply.github.com>
2021-01-14 17:07:37 +0100

    Update post.py

--- schemas/post.py
+++ schemas/post.py
@@ -1,14 +1,14 @@
 from ma import ma
 from models.post import PostModel
 
-class PostSchema(ma.Schema):
+class PostSchema(ma.SQLAlchemySchema):
 
     class Meta:
         model = PostModel
         dump_only = ("post_id","login", "date")
 
 
-class PostUpdateSchema(ma.Schema):
+class PostUpdateSchema(ma.SQLAlchemySchema):
 
     class Meta:
         model = PostModel
